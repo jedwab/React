3d6c986e644cb9263f6a22101d562a635657fc9c
Justyna Bobrowska <justynabobrowska88@gmail.com>
2018-02-04 12:19:41 +0100

    first react exercise

--- script.js
+++ script.js
@@ -1,2 +1,36 @@
-var element = React.createElement('div', {}, 'Hello world!');
-ReactDOM.render(element, document.getElementById('app'));
+var movies = [
+  {
+  	id: 1,
+    title: 'Vikings',
+    desc: 'Historyczno-przygodowy',
+    img: 'images/vikings.jpg'
+  },
+  {
+  	id: 2,
+    title: 'Sherlock Holmes',
+    desc: 'Historyczno-przygodowy',
+    img: 'images/sherlock.jpg'
+  },
+  {
+  	id: 3,
+  	title: 'Interstellar',
+  	desc: 'Science-Fiction',
+  	img: 'images/interstellar.jpg'
+  }
+];
+
+var moviesElements = movies.map(function(movie) {
+  return React.createElement('li', {key: movie.id},
+      React.createElement('h2', {}, movie.title),
+      React.createElement('p', {}, movie.desc),
+      React.createElement('img', {src: movie.img})
+    );
+});
+
+var element =
+  React.createElement('div', {},
+    React.createElement('h1', {}, 'Lista filmów'),
+    React.createElement('ul', {}, moviesElements)
+  );
+
+  ReactDOM.render(element, document.getElementById('app'))
